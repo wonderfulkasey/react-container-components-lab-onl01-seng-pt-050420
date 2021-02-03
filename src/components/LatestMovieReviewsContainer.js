df20639c3745083279d4949d8c2bef7f595097fb
Kasey Arnold <kaseyarnold15@gmail.com>
2021-02-03 21:33:42 +0000

    Automatically backed up by Learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -8,3 +8,29 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
 
 // Code LatestMovieReviewsContainer Here
 
+export default class LatestMovieReviewsContainer extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      reviews: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(URL)
+      .then(response => response.json())
+      .then(reviewData => this.setState({ reviews: reviewData.results }))
+      //.then(reviewData => console.log(reviewData.results));
+  }
+
+  render() {
+    return (
+      <div className="latest-movie-reviews">
+        <h2>The Latest Reviews:</h2>
+        <MovieReviews reviews={this.state.reviews}/>
+      </div>
+    )
+  }
+}
